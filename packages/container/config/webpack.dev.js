8db222bd983600ed113231a6953a542d7903094f
Arora, Harshit (Hiring Our Heroes fellowship) <Arora_Harshit@ne.bah.com>
2024-06-19 22:30:12 -0700

    Deploying container code after some change

--- packages/container/config/webpack.dev.js
+++ packages/container/config/webpack.dev.js
@@ -2,7 +2,7 @@ const { merge } = require('webpack-merge');
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
-
+const delete_this ="delet_this"
 const devConfig = {
   mode: 'development',
   devServer: {
